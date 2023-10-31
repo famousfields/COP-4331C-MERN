a82b07cdd1b6ad262e56e46a51c8262765d3ac78
famousfields <willfields92@gmail.com>
2023-10-31 15:32:53 -0400

    added routes and attemped to center login form lol

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,44 @@
-import logo from './logo.svg';
-import {useState, useEffect} from 'react'
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import Signup from './Pages/Signup';
+import About from './Pages/About';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import "./App.css";
 
 function App() {
-  const [backendData,setBackendData] = useState([{}])
-  useEffect(()=>
-  {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => 
-      {
-        setBackendData(data);
-      }
-    )
-  },[])
+  // const [backendData,setBackendData] = useState([{}])
+  // useEffect(()=>
+  // {
+  //   fetch("/api").then(
+  //     response => response.json()
+  //   ).then(
+  //     data => 
+  //     {
+  //       setBackendData(data);
+  //     }
+  //   )
+  // },[])
 
   return (
-    <div >
-     {typeof backendData.users === 'undefined' ? (<p>Loading...</p>) : (backendData.users.map((user,i) =>
-     (<p key = {i}>{user}</p>)) 
-     )}
-    </div>
+    // <div >
+    //  {typeof backendData.users === 'undefined' ? (<p>Loading...</p>) : (backendData.users.map((user,i) =>
+    //  (<p key = {i}>{user}</p>)) 
+    //  )}
+    // </div>
+    <>
+      <div className='App'>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path = "/" element = {<Home/>}/>
+            <Route path = "/signup" element = {<Signup/>}/>
+            <Route path = "/login" element = {<Login/>}/>
+            <Route path = "/about" element = {<About/>}/>
+          </Routes>
+        </BrowserRouter>
+        
+      </div>
+    </>
+      
   );
 }
 
--- front-end/src/Pages/Login.js
+++ front-end/src/Pages/Login.js
@@ -1,4 +1,5 @@
 import React, {useState,useEffect} from 'react'
+import { useHistory } from "react-router-dom";
 
 function Login() 
 {
@@ -8,60 +9,69 @@ function Login()
     const [validUserData,setValidUserData] = useState(false);
     const [badPass,setBadPass] = useState(false);
     const [userData,setUserData] = useState([{}]);
-    const [errorMessage,setErrorMessage] = usestate("");
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        const body = JSON.stringify({email,password});
-        console.log(body);
+    const [errorMessage,setErrorMessage] = useState("");
+    const [isSubmitted,setIsSubmitted] = useState(false);
 
-        const response = await fetch("/api")
-        let json;
-        try 
-        {
-            json = await response.json();
-        }
-        catch (e)
-        {
-            console.error(e);
-            setErrorMessages({name: "json", message: "The response from the server could not be parsed."})
-            setIsSubmitted(false);
-            return;
-        }
-        if(response.ok){
-            setValidUserData(true);
-            setErrorMessage(null);
-            data => {
-                setUserData(data);
-            }
-        }
-        else
-        {
-            //check which field in form is incorrect and adjust accordingly
-        }
+    const handleSubmit = async(e) => {
+        // e.preventDefault();
+        // const body = JSON.stringify({email,password});
+        // console.log(body);
+
+        // const response = await fetch("/api")
+        // let json;
+        // try 
+        // {
+        //     json = await response.json();
+        // }
+        // catch (e)
+        // {
+        //     console.error(e);
+        //     setErrorMessage({name: "json", message: "The response from the server could not be parsed."})
+        //     setIsSubmitted(false);
+        //     return;
+        // }
+        // if(response.ok){
+        //     setValidUserData(true);
+        //     setErrorMessage(null);
+        //     data => {
+        //         setUserData(data);
+        //     }
+        // }
+        // else
+        // {
+        //     //check which field in form is incorrect and adjust accordingly
+        // }
+    }
+
+    const redirectSignUp = () => {
+        return window.location.assign("/signup");
     }
 
     const loginForm = (
-        <form onSubmit={handleSubmit}>
-            <label> Email
-                <input
-                    type='text'
-                    required
-                    value={email}
-                    name='email'
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </label>
-            <label> password
-                <input
-                    type='text'
-                    required
-                    value={password}
-                    name='pass'
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </label>
-            <input type='submit' value= "login" />
-        </form>
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <label> Email
+                    <input
+                        type='text'
+                        required
+                        value={email}
+                        name='email'
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <label> password
+                    <input
+                        type='text'
+                        required
+                        value={password}
+                        name='pass'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <input type='submit' value= "login" />
+                <button onClick={redirectSignUp}>   Sign up</button>
+            </form>
+        </div>
     );
   return (
    loginForm
