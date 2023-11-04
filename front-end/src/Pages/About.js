35be624373db4cbab5ea8732e781a46896e6bc47
famousfields <willfields92@gmail.com>
2023-11-04 10:58:05 -0400

    added navbar + content on about page

--- front-end/src/Pages/About.js
+++ front-end/src/Pages/About.js
@@ -2,8 +2,32 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About
+    <div className='about-devs'>
+      <div className='dev-entry'>
+        <h1>William Fields</h1>
+        <h3>Front-end</h3>
+        <h4>Love front-end</h4>
+      </div>
+      <div className='dev-entry'>
+        <h1>Marco Andres Saavedra</h1>
+        <h3>Data Base / Application programming interface</h3>
+      </div>
+      <div className='dev-entry'>
+        <h1>Nathan Rodriguez</h1>
+        <h3>Data Base</h3>
+      </div>
+      <div className='dev-entry'>
+        <h1>Jason Griller</h1>
+        <h3>Application programming interface</h3>
+      </div>
+      <div className='dev-entry'>
+        <h1>Christian Rodriguez</h1>
+        <h3>Data Base</h3>
+      </div>
+      <div className='dev-entry'>
+        <h1>David Patenaude</h1>
+        <h3>Front-end</h3>
+      </div>
     </div>
   )
 }
--- front-end/src/Pages/UserExpenses.js
+++ front-end/src/Pages/UserExpenses.js
@@ -11,17 +11,22 @@ const [expenses,setExpenses] = useState({});
       <div className='expenses'>
         <div className='expense'>
           <div className='text'>Gas</div>
+          <div className='price'>Price: $150</div>
           <div className='delete-expense'>x</div>
         </div>
         <div className='expense'>
           <div className='text'>food</div>
+          <div className='price'>Price: $500</div>
           <div className='delete-expense'>x</div>
         </div>
         <div className='expense'>
           <div className='text'>phone bill</div>
+          <div className='price'>Price: $50</div>
           <div className='delete-expense'>x</div>
         </div>
       </div>
+      <div className='expense-total'>Expense Total: $700</div>
+      <button>add expense</button>
     </div>
   )
 }
