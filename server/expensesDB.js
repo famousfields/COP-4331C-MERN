28d31b062f9ea95dcb9ffa3bcc025b1049ecb1e5
famousfields <willfields92@gmail.com>
2023-11-16 09:27:10 -0500

    delete uneeded commponent

--- server/expensesDB.js
+++
@@ -1,9 +0,0 @@
-const { Int32 } = require('mongodb');
-const mongoose = require('mongoose');
-
-const expenseSchema = new mongoose.Schema({
-    expense: String,
-    price: Int32
-});
-
-module.exports = mongoose.model('userExpense',expenseSchema);
