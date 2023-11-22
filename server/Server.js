866ede3a8c4d90c74e60094cb15faf1306c4b6bc
MarkMango21 <mark2asf@gmail.com>
2023-11-22 15:42:18 -0800

    Final debug

--- server/Server.js
+++ server/Server.js
@@ -1,4 +1,5 @@
-var express = require('express');
+//var express = require('express');
+const express = require('express');
 //var MongoClient = require("mongodb").MongoClient;
 const mongodb = require('mongodb');
 var cors = require('cors');
@@ -7,7 +8,7 @@ var multer = require("multer");
 const connectDB = require("./dbConn");
 const User = require("./models/userModel");
 const Expense = require('./models/expenseModel');
-//const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 // Other imports
 // ...
@@ -16,6 +17,7 @@ const Expense = require('./models/expenseModel');
 
 
 // const { default: UserExpenses } = require('../front-end/src/Pages/UserExpenses');
+
 
 var app = express();
 const PORT  = 5000;
